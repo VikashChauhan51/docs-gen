6f12daaddf3c29bbf18ef27404ef1273809773d9
Vikash Chauhan <vikashchauhan51@gmail.com>
2023-07-15 21:43:00 +0530

    added builder

---
+++ src/DocsGen/DocGeneratorBuilder.cs
@@ -0,0 +1,24 @@
+﻿
+using DocsGen.Core;
+
+namespace DocsGen;
+public class DocGeneratorBuilder
+{
+
+    private readonly IDictionary<DocType, Func<DocGenerator>> docGenerators = new Dictionary<DocType, Func<DocGenerator>>();
+
+    public DocGeneratorBuilder Register<T>(DocType docType) where T : DocGenerator, new()
+    {
+        docGenerators[docType] = () => new T();
+        return this;
+    }
+    public DocGenerator Build(DocType docType)
+    {
+        if (docGenerators.TryGetValue(docType, out var generatorFactory))
+        {
+            return generatorFactory();
+        }
+
+        throw new ArgumentException("Invalid DocType");
+    }
+}
